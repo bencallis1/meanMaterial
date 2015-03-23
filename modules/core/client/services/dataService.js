a4f4604ff499b4f87e5250fbf30267e7fac62b62
bencallis <bencallis1@gmail.com>
2015-03-23 11:15:14 -0600

    Added $mdSideNav to the landings page.

--- modules/core/client/services/dataService.js
+++ modules/core/client/services/dataService.js
@@ -2,19 +2,11 @@
 angular.module('core').service('usersService', ['$q',
     function($q) {
  
- 
-    /**
-     * Users DataService
-     * Uses embedded, hard-coded data model; acts asynchronously to simulate
-     * remote data service call(s).
-     *
-     * @returns {{loadAll: Function}}
-     * @constructor
-     */
+
  
     var users = [
         {
-            name: 'Ben Callis',
+            name: 'Home',
             nav: 'svg-1',
             content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
         },
