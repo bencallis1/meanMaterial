510c870e22cc5c52e3164fd6f69d9e2db95127cf
bencallis <bencallis1@gmail.com>
2015-03-24 08:39:46 -0600

    added background to HP and some css to reset. BCstyles needs some work in order to work with the $md layout. Remove any fixed width and add flex

--- modules/core/client/app/init.js
+++ modules/core/client/app/init.js
@@ -9,12 +9,17 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
         $locationProvider.html5Mode(true).hashPrefix('!');
  
         $mdThemingProvider.theme('default')
-            .primaryPalette('purple')
-            .accentPalette('red');
+            .primaryPalette('grey', {
+                'default':'700'
+            })
+            .accentPalette('cyan', {
+                'default': 'A100'
+            });
  
         // Register the user `avatar` icons
         $mdIconProvider
-            .defaultIconSet('./assets/svg/avatars.svg', 128)
+            .defaultIconSet('./assets/svg/avatars.svg', 128 )
+
             .icon('menu'       , './assets/svg/menu.svg'        , 24)
             .icon('share'      , './assets/svg/share.svg'       , 24)
             .icon('google_plus', './assets/svg/google_plus.svg' , 512)
--- modules/core/client/services/dataService.js
+++ modules/core/client/services/dataService.js
@@ -7,34 +7,32 @@ angular.module('core').service('usersService', ['$q',
     var users = [
         {
             state: 'landings.home',
-            name: 'Home',
             text:'hello from the service',
             nav: 'svg-1',
             content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
         },
         {
             state: 'userNotes',
-            name: 'Notifacations',
             nav: 'svg-2',
             content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
         },
         {
-            name: 'Calendar',
+
             nav: 'svg-3',
             content: 'Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeneys blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS.'
         },
         {
-            name: 'Search',
+
             nav: 'svg-4',
             content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
         },
         {
-            name: '',
+
             nav: 'svg-5',
             content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
         },
         {
-            name: 'Logout',
+
             nav: 'svg-6',
             content: 'Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new drivers license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You dont go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada.'
         }
